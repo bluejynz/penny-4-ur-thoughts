7f9d89e3082c9bfc6815b1d101c0e3e1718b9569
Dany Falk <dany_falk@hotmail.com>
2025-01-16 17:36:58 -0300

    port config into server

--- src/server.ts
+++ src/server.ts
@@ -2,25 +2,27 @@ import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
 import { routes } from "./routes";
 
+const port = Number(process.env.PORT) || 3333;
+
 const app = fastify({ logger: true });
 
 app.setErrorHandler((error, request, reply) => {
-  if (error.name === "NotFoundError") {
-    reply.code(404).send({ message: error.message });
-  } else {
-    reply.code(400).send({ message: error.message });
-  }
+    if (error.name === "NotFoundError") {
+        reply.code(404).send({ message: error.message });
+    } else {
+        reply.code(400).send({ message: error.message });
+    }
 });
 
 const start = async () => {
-  await app.register(fastifyCors);
-  await app.register(routes);
+    await app.register(fastifyCors);
+    await app.register(routes);
 
-  try {
-    await app.listen({ port: 3333 });
-  } catch (error) {
-    process.exit(1);
-  }
+    try {
+        await app.listen({ port, host: "0.0.0.0" });
+    } catch (error) {
+        process.exit(1);
+    }
 };
 
 start();
